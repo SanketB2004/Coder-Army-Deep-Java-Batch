24203f340f1b8e41bd310d28f863d9e5492f32de
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-08 19:30:34 +0530

    Commit

--- Strings/string01.java
+++ Strings/string01.java
@@ -7,17 +7,19 @@ public class string01 {
 
     public static void main(String[] args) {
         // String literal 
-String s1 = "Sanket";
-String s2 = "Sanket";
+// String s1 = "Sanket";
+// String s2 = "Sanket";
 
-System.out.println(s1==s2);
+// System.out.println(s1==s2);
+ 
+// // String new use keyword 
 
-// String new use keyword 
+// String s3 = new String("Hello");// using new operator
+// String s4 = new String("Hello");
 
-String s3 = new String("Hello");
-String s4 = new String("Hello");
+// System.out.println(s3.equals(s4));
 
-System.out.println(s3==s4);
+
     }
     
 }
