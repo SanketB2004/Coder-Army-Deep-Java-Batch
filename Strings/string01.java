7b7a6acf72f23ca39dcdf4fc2764b49e50e49dbb
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-08 19:11:26 +0530

    Commit

--- Strings/string01.java
+++ Strings/string01.java
@@ -6,9 +6,18 @@ package Strings;
 public class string01 {
 
     public static void main(String[] args) {
-        
+        // String literal 
+String s1 = "Sanket";
+String s2 = "Sanket";
 
-        
+System.out.println(s1==s2);
+
+// String new use keyword 
+
+String s3 = new String("Hello");
+String s4 = new String("Hello");
+
+System.out.println(s3==s4);
     }
     
 }
