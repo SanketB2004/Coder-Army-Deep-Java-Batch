baab7470570dea36619e13774b6a92d49b391535
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-09 18:49:49 +0530

    Commit

--- Strings/string03.java
+++ Strings/string03.java
@@ -4,6 +4,18 @@ public class string03 {
     
     public static void main(String[] args) {
         
-        
+//   String s1 = new String();
+//   String s2 = new String("Heloo");
+// //   System.out.println(s2); 
+
+//   String s3 = "Sanket";
+
+//   String s4 = new String(s3);
+  char[] arr = {'S','A','N','K','E','T'};
+  String s6 = new String(arr,0,2);
+System.out.println(s6); // this 0 is index and count is 2 index tak print kara words 
+
+
+
     }
 }
