8ad56e164dfdb572d7ab2f493728dc8252c4272f
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-12 18:56:22 +0530

    Commit

--- clone.java
+++ clone.java
@@ -1,12 +1,12 @@
 public class clone {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException { /// exception handle 
         
         
 
     }
     protected Object clone() throws CloneNotSupportedException{
 
-        return super.clone();
+        return super.clone(); // that is a this methods used to make clone
     }
 }
