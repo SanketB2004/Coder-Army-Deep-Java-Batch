5df4d0d77615551a53337e614f7d1c1cabe8e671
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-12 18:54:50 +0530

    Commit

---
+++ clone.java
@@ -0,0 +1,12 @@
+public class clone {
+    
+    public static void main(String[] args) {
+        
+        
+
+    }
+    protected Object clone() throws CloneNotSupportedException{
+
+        return super.clone();
+    }
+}
