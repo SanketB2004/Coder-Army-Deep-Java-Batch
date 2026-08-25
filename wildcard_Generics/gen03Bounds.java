7980c3a5e04ecb99acbec8e62a05d5c545c94a2e
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-25 19:34:26 +0530

    Commit

---
+++ wildcard_Generics/gen03Bounds.java
@@ -0,0 +1,36 @@
+package wildcard_Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+// wildcards with upperbounds
+public class gen03Bounds {
+    public static void main(String[] args) {
+         List<dog> dogs = new ArrayList<>();
+
+        dogs.add(new dog());
+        dogs.add(new dog());
+fun(dogs);
+    }
+    static void fun(List<? extends Animal> values) {
+       for (Animal animal : values) {
+        animal.eat();
+       }
+    } 
+}
+
+class Animal {
+    void eat() {
+        System.out.println("Eating");
+    }
+
+    void walk() {
+        System.out.println("Walking");
+    }
+
+}
+
+class dog extends Animal {
+    void bark() {
+        System.out.println("Barking");
+    }
+}
