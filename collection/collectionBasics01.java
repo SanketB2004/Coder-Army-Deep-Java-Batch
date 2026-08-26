b8f88508d88674437ca6994dcfeefd27929754f4
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-26 19:41:00 +0530

    Commit

---
+++ collection/collectionBasics01.java
@@ -0,0 +1,20 @@
+package collection;
+
+// This is collection basics and introduction code 
+import java.util.*; // this statement import java pacakage
+
+class collectionBasics01 {
+
+    public static void main(String[] args) {
+// Collection 
+// List Example 
+
+List<Integer> arr = List.of(1,2,3,4,5,6,7,8,9,10);
+
+for (Integer integer : arr) {
+    System.out.print(integer + " ");
+}System.out.println();
+
+    }
+
+}
