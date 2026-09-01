c5678657598584de36d346baf7bbfaccffdd826c
SanketB2004 <bhosalesanket1507@gmail.com>
2026-09-01 19:19:09 +0530

    Commit

---
+++ collection/collectionMethods.java
@@ -0,0 +1,12 @@
+package collection;
+// This Class Used A Only Collection Methods
+/**
+ * collectionMethods
+ */
+public class collectionMethods {
+public static void main(String[] args) {
+    
+    
+}
+    
+}
