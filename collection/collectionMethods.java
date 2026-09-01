468f5f5cc756bee815777c5fbba808280393bfe6
SanketB2004 <bhosalesanket1507@gmail.com>
2026-09-01 19:22:44 +0530

    Commit

--- collection/collectionMethods.java
+++ collection/collectionMethods.java
@@ -3,10 +3,23 @@ package collection;
 /**
  * collectionMethods
  */
+import java.util.*;
+
 public class collectionMethods {
 public static void main(String[] args) {
     
-    
+    Collection<Integer> c = new ArrayList<>();
+c.add(1);// Add Method
+c.add(2);// Add Method
+c.add(3);// Add Method
+
+// size method 
+int n = c.size();
+System.out.println(n);
+// is empty method 
+System.out.println(c.isEmpty());
+
+
 }
     
 }
