5c74cca9f12c4386c3ab4dd32e3724cfc4fa2943
SanketB2004 <bhosalesanket1507@gmail.com>
2026-09-09 19:05:51 +0530

    Commit

--- SETFILECOLLECTION/basic.java
+++ SETFILECOLLECTION/basic.java
@@ -5,7 +5,22 @@ public class basic {
     public static void main(String[] args) {
 
 // constructor of hashset/linkedhashset 
-Set<Integer> set = new HashSet<>();
+Set<Integer> set = new LinkedHashSet<>();
+
+// initial capasity 
+Set<Integer> set1 = new LinkedHashSet<>(16);
+
+// capasity , load factor 
+Set<Integer> set2 = new LinkedHashSet<>(16 , 0.75f);
+
+// use another collection 
+// direct in element add a in set using another collection 
+ 
+Set<Integer> set3 = new LinkedHashSet<>(List.of(1,2,3,4,5,6));
+
+
+// Tree Set Using 
+
 
 
 
