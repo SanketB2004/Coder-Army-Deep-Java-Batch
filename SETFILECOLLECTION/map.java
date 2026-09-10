87cecd33beda442e6be6b8f4adfb9364b770d14c
SanketB2004 <bhosalesanket1507@gmail.com>
2026-09-10 15:24:58 +0530

    Commit

--- SETFILECOLLECTION/map.java
+++ SETFILECOLLECTION/map.java
@@ -12,17 +12,17 @@ public class map {
 map.size();
 map.isEmpty();
 
-System.out.println(map.containsValue("Sanket"));// check value exits
-System.out.println(map.containsKey(101)); // check a key are exits
+//System.out.println(map.containsValue("Sanket"));// check value exits
+//System.out.println(map.containsKey(101)); // check a key are exits
 
-System.out.println(map.get(101));// element get this all map
+//System.out.println(map.get(101));// element get this all map
 
 // // update 
- System.out.println(map.put(106 , "Raje"));
+ //System.out.println(map.put(106 , "Raje"));
 // sout madhe ahe lagicha update ny hot to retur null denar check in terminal 
 // remove 
 // map.remove(105);
-System.out.println(map.get(106));
+//System.out.println(map.get(106));
 
 // map.putAll() bug values put in map 
 
@@ -43,6 +43,18 @@ System.out.println(map.get(106));
 // map.replace (104 , "Sanket") only update 
 
 // map.replace(104 , sanket , SANKET) mhanje ki key 104 madhe jr sanket asel taracha me update karun SANKET takanar
+
+ Set<Map.Entry<Integer , String>> entries = map.entrySet();
+
+
+// for (Map.Entry<Integer,String> entry : entries) {
+//     Integer key = entry.getKey();
+//     String value = entry.getValue();
+
+//     System.out.println(key + " , "+value);
+// }
+
+Map<Integer , String> map8 = Map.of(101,"sanket" , 102 , "kaka", 103 , "aai" , 104 , "Bala");
 
 
 
