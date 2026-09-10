972cc8cbc082b010acf33f996be74d401f6690f0
SanketB2004 <bhosalesanket1507@gmail.com>
2026-09-10 15:38:07 +0530

    Commit

---
+++ SETFILECOLLECTION/treemap.java
@@ -0,0 +1,14 @@
+import java.util.TreeMap;
+
+public class treemap {
+    public static void main(String[] args) {
+        
+        TreeMap<Integer , String> map = new TreeMap<>();
+
+        map.put(101, "Sanket");
+   map.put(102, "Kaka");
+   map.put(103, "Aai");
+   map.put(104, "Atharv");
+   map.put(105, "Bhosale"); 
+    }
+}
